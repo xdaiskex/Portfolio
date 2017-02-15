2152b6c2516fc3f4a0f589284aa3077650b4e748
Anthony Nguyen <anthonypn93@gmail.com>
2017-02-15 14:35:47 -0500

    Full screen mobile nav

--- js/init.js
+++ js/init.js
@@ -1,6 +1,9 @@
 (function($){
 	$(function(){
-		$('.button-collapse').sideNav();
+		$('.button-collapse').sideNav({
+	      	closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	  		}
+	  	);
 		$('.parallax').parallax();
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
